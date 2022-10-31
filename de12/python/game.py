94e6072e8e5bd517fb3fe2734c49507925db16eb
makusae03 <r202202191er@jindai.jp>
2022-11-01 01:16:33 +0900

    1031

---
+++ de12/python/game.py
@@ -0,0 +1,53 @@
+import time  #1問目
+
+t = 10 
+
+while t>0:
+  print(t)
+  time.sleep(1)
+  t -= 1
+
+
+
+import time  #2問目
+
+t = 10
+
+while t>0:
+  print(t)
+  time.sleep(1)
+  t -= 1
+
+
+
+import time #3問目
+
+t = 15 
+
+while t>0:
+  print(t)
+  time.sleep(1)
+  t -= 1
+
+
+
+import time #4問目
+
+t = 15 
+
+while t>0:
+  print(t)
+  time.sleep(1)
+  t -= 1
+  
+
+
+import time #5問目
+
+t = 30
+
+while t>0:
+  print(t)
+  time.sleep(1)
+  t -= 1
+ 
---
+++ de12/python/work1.py
@@ -0,0 +1,8 @@
+name="doyo"
+waist=84
+age=44
+
+
+print(name,"さんは腹囲",waist,"cmで年齢は",age,"才ですね")
+
+# ageという変数に44を代入している
