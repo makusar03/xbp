bf96f2e891a1f32e2996f31d87eec2818ed143ba
makusae03 <r202202191er@jindai.jp>
2022-11-08 15:23:54 +0900

    1

--- de12/python/kabu.py
+++ de12/python/kabu.py
@@ -1,2 +1,8 @@
 import webbrowser
 webbrowser.open('https://www.nikkei.com/nkd/industry/stocklist/?n_m_code=101') 
+a = ["stock.py", "systemsoftware.py", "retail.py", "finamce.py", "massmedia.py"]
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
